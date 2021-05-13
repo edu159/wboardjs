d18cbe08bd8bdd745d454b012b188e53d7a1e6ef
Peter (dkdndes) <dkdndes@gmail.com>
2021-05-13 19:27:42 +0200

    feature: update for django 3 and applied black

--- WBoardJS/apps.py
+++ WBoardJS/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class WboardjsConfig(AppConfig):
-    name = 'WBoardJS'
+class wboardjsConfig(AppConfig):
+    name = "wboardjs"
